2a89b6ca6259729f69281c049aa535f5a1c84a65
Igor Krupenja <igor.krupenja@gmail.com>
2019-01-28 15:57:47 +0200

    Store VSCode GPG key in /tmp

--- install.sh
+++ install.sh
@@ -73,8 +73,8 @@ install_sw_apt() {
     apt-get install -y apt-transport-https curl
 
     # VSCode
-    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >microsoft.gpg
-    install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /tmp/microsoft.gpg
+    install -o root -g root -m 644 /tmp/microsoft.gpg /etc/apt/trusted.gpg.d/
     sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
     apt-get update -y
     # Sublime Merge
