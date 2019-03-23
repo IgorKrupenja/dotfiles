8889507ba6a709faa02132e5f2e1e354212058c6
Igor Krupenja <igor.krupenja@gmail.com>
2019-03-23 22:00:14 +0200

    Update VSCode extensions

--- VSCode/extensions.sh
+++ VSCode/extensions.sh
@@ -17,6 +17,7 @@ code --install-extension formulahendry.auto-rename-tag
 code --install-extension foxundermoon.shell-format
 code --install-extension gio00.convertplist
 code --install-extension Gruntfuggly.todo-tree
+code --install-extension HookyQR.beautify
 code --install-extension idleberg.applescript
 code --install-extension James-Yu.latex-workshop
 code --install-extension kokororin.vscode-phpfmt
