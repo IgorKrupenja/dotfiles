427f39fb990cb15031bbe961189d45804b60cdf1
Igor Krupenja <igor.krupenja@gmail.com>
2019-08-15 18:08:41 +0300

    Another attempt to fix omz theme

--- install.sh
+++ install.sh
@@ -152,7 +152,7 @@ zsh_config() {
     # Install oh-my-zsh
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
     # Install theme
-    sudo git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+    git clone https://github.com/romkatv/powerlevel10k.git /home/igor/.oh-my-zsh/custom/themes/powerlevel10k
     # Install plug-ins
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
