92e37a1501f2a8205147edf835b79177ad15805b
Igor Krupenja <igor.krupenja@gmail.com>
2019-08-15 17:43:43 +0300

    Fix oh-my-zsh removal

--- install.sh
+++ install.sh
@@ -147,7 +147,7 @@ install_sw_misc_macos() {
 
 zsh_config() {
     # Remove any existing install first
-    rm -r /home/igor/.oh-my-zsh
+    rm -rf /home/igor/.oh-my-zsh
     # Install oh-my-zsh
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     # Install plug-ins
